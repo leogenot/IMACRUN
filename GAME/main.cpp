2c03182bbcfefcd2736b0e68827885242b43cc13
Léo Genot <45610291+leogenot@users.noreply.github.com>
2021-11-24 15:46:16 +0100

    auto move forward

--- GAME/main.cpp
+++ GAME/main.cpp
@@ -146,12 +146,9 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        camera->ProcessKeyboard(FORWARD, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-        camera->ProcessKeyboard(BACKWARD, deltaTime);
-    }
+    
+    camera->ProcessKeyboard(FORWARD, deltaTime);
+
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
         camera->ProcessKeyboard(LEFT, deltaTime);
     }
--- lib/GAME_H/trackballCamera.hpp
+++ lib/GAME_H/trackballCamera.hpp
@@ -59,7 +59,7 @@ public:
         glm::mat4 Translate = glm::translate(glm::mat4(1.f), glm::vec3(0, 0, -m_fDistance));
         glm::mat4 RotateX = glm::rotate(glm::mat4(1.f), glm::radians(m_fAngleX), glm::vec3(1, 0, 0));
         glm::mat4 RotateY =  glm::rotate(glm::mat4(1.f), glm::radians(m_fAngleY), glm::vec3(0, 1, 0));
-        glm::mat4 LookTo = glm::translate(glm::mat4(1.f), glm::vec3(0, 0, 0));
+        glm::mat4 LookTo = glm::translate(glm::mat4(1.f), glm::vec3(3, 0, 0));
         glm::mat4 ViewMatrix = glm::mat4(Translate * RotateX * RotateY * LookTo);
     
         return ViewMatrix;
