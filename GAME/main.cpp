520dfd856338a96101e676bec9cb04e4bca37e79
Léo Genot <45610291+leogenot@users.noreply.github.com>
2021-12-06 14:04:09 +0100

    main menu update (missing load, save and options)

--- GAME/main.cpp
+++ GAME/main.cpp
@@ -29,8 +29,9 @@ bool   fixedCamera = false;
 bool MouseIn  = false;
 bool MouseOut = true;
 
-bool show_demo_window    = false;
-bool show_another_window = false;
+bool show_main_menu_window    = false;
+bool show_quit_window = false;
+bool show_options_window = false;
 
 bool paused = false;
 
@@ -173,38 +174,80 @@ int main()
             ImGui::NewFrame();
 
             // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-            if (show_demo_window)
-                ImGui::ShowDemoWindow(&show_demo_window);
+            /* if (show_main_menu_window)
+                ImGui::ShowDemoWindow(&show_main_menu_window); */
 
             // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-            if (show_demo_window) {
-                static float f       = 0.0f;
-                static int   counter = 0;
+            if (show_main_menu_window) {
+                static bool             use_work_area = true;
+                static ImGuiWindowFlags flags         = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings;
 
-                ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+                // We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+                // Based on your use case you may want one of the other.
+                const ImGuiViewport* viewport = ImGui::GetMainViewport();
+                ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+                ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
+                ImGui::Begin("Main Menu IMACRUN", &show_main_menu_window, flags);
 
-                ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
-                ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
+                if (ImGui::Button("New Game")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                    std::cout << "hello" << std::endl;
 
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+                if (ImGui::Button("Resume Game")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                {
+                    show_main_menu_window = false;
+                    paused           = !paused;
+                }
 
-                if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
-                    counter++;
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
+                if (ImGui::Button("Save Game")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                    std::cout << "hello" << std::endl;
+
+                if (ImGui::Button("Load Game")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                    std::cout << "hello" << std::endl;
+
+                if (ImGui::Button("Options")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                    show_options_window = true;
+
+                if(ImGui::Button("Quit Game"))
+                    show_quit_window = true; // Buttons return true when clicked (most widgets return true when edited/activated)
 
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
                 ImGui::End();
             }
 
-            // 3. Show another simple window.
-            if (show_another_window) {
-                ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-                ImGui::Text("Hello from another window!");
-                if (ImGui::Button("Close Me"))
-                    show_another_window = false;
+            // Options confirmation window
+            if (show_options_window) {
+                static bool             use_work_area = true;
+                static ImGuiWindowFlags flags         = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings;
+
+                // We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+                // Based on your use case you may want one of the other.
+                const ImGuiViewport* viewport = ImGui::GetMainViewport();
+                ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+                ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
+
+                ImGui::Begin("Game options", &show_options_window, flags);
+                if (ImGui::Button("Back to main menu"))
+                    show_options_window = false;
+                ImGui::End();
+            }
+
+            // Exit confirmation window
+            if (show_quit_window) {
+                static bool             use_work_area = true;
+                static ImGuiWindowFlags flags         = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings;
+
+                // We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+                // Based on your use case you may want one of the other.
+                const ImGuiViewport* viewport = ImGui::GetMainViewport();
+                ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+                ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
+
+                ImGui::Begin("Quit Game", &show_quit_window, flags); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+                ImGui::Text("Are you sure you want to quit game?");
+                if (ImGui::Button("NO stay here"))
+                    show_quit_window = false;
+                if (ImGui::Button("YES quit game"))
+                    glfwSetWindowShouldClose(window, true);
                 ImGui::End();
             }
             // Renders the ImGUI elements
@@ -233,13 +276,13 @@ void processInput(GLFWwindow* window)
     if (newStatePause == GLFW_RELEASE && oldStatePause == GLFW_PRESS) {
         if (paused) {
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-            show_demo_window = !show_demo_window;
-            paused = false;
+            show_main_menu_window = !show_main_menu_window;
+            paused           = false;
         }
         else {
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-            paused = true;
-            show_demo_window = !show_demo_window;
+            paused           = true;
+            show_main_menu_window = !show_main_menu_window;
         }
     }
     oldStatePause = newStatePause;
