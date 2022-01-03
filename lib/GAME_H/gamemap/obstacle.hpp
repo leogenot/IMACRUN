c525920f9ad34ed1de6a8867c26b7a737760248f
Candice Branchereau <candicebranchereau@gmail.com>
2022-01-03 18:46:00 +0100

    destructor obstacle

--- lib/GAME_H/gamemap/obstacle.hpp
+++ lib/GAME_H/gamemap/obstacle.hpp
@@ -15,6 +15,7 @@ private:
 
 public:
     Obstacle(glm::vec3 pos, unsigned int texture);
+    ~Obstacle() { glDeleteVertexArrays(1, &m_VAO); };
     void draw(glm::mat4 view, glm::mat4 projection, glm::mat4 model, glm::vec3 camPos, SceneLight sceneLight, std::vector<Light*> lights, glm::vec3 playerPos, int renderRadius) const;
     glm::vec3 getPos() const {return m_pos;}; 
 };
